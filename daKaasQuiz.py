9819428c5d1d65447b124c71032d4d9db1a13e6d
Idioot <99065788@mydavinci.nl>
2021-09-09 14:56:00 +0200

    vuilisman

---
+++ daKaasQuiz.py
@@ -0,0 +1,20 @@
+print('welkom bij de kaaaaass quiizzzzz')
+print('weeee beginnennnn!!!')
+print()
+
+if input('Is de kaas geel? ') == 'ja':
+    if input('heeft de kaas gaten? ') == 'ja':
+        if input('is de kaas belachelijk duur? ') == 'ja':
+            print('de kaas is emmenthaler')
+        else: print('de kaas is Leerdammer')
+    elif input('is de kaas hard als steen? ') == 'ja':
+        print('de kaas is Panmigiano Reggiano')
+    else: print('de kaas is Goudese kaas')
+elif input('heeft de kaas blauwe schimmels? ') == 'ja':
+    if input('heeft de kaas een korst? ') == 'ja':
+        print('de kaas is Bleu de Rochbaron')
+    else: print("de kaas is Founme d'Amber")
+elif input('heeft de kaas een korst? ') == 'ja':
+    print('de kaas is Camembert')
+else: print ("de kaas is mozzarella")
+
---
+++ sollicitatie.py
@@ -0,0 +1,26 @@
+print('vacature ruimtevuilnisman')
+print()
+if input('hoeveel praktijkervaring heeft u met dierendressuur? ') > '4':
+    if input('welke diploma heeft u? ') == 'MBO-4 ondernemen':
+      if input('Typ een salto! ') != '|/-\|':
+        if input('kan u vliegend water drinken? ') != 'fdfsfs':
+            if input('ben je ooit in de ruimte geweest? ') != 'fjhsgj':
+                if input('hoeveel kinderen heeft u? ') != 'fgksjhgf':
+                    if input('bezit u een Vrachtwagen rijbewijs? ja of nee? ') == 'ja':
+                        if input('bent u man met een snor breder dan 10cm of bent u vrouw en draagt rood krulhaar langer dan 20cm? ja of nee? ') == 'ja': 
+                            if input('hoe lang bent u? in hele cm ') > '150':
+                                if input('hoe zwaar bent u? in hele KG ') > '90':
+                                    if input('heeft e een Certificaat "Overleven met gevaarlijk personeel?" ja of nee? ') == 'ja':
+                                       print('gefeliciteerd u bent aangenomen')    
+                                    else: print('helaas u voldoet niet aan de eisen')  
+                                else: print('helaas u voldoet niet aan de eisen') 
+                            else: print('helaas u voldoet niet aan de eisen') 
+                        else: print('helaas u voldoet niet aan de eisen') 
+                    else: print('helaas u voldoet niet aan de eisen') 
+                else: print('helaas u voldoet niet aan de eisen') 
+            else: print('helaas u voldoet niet aan de eisen') 
+        else: print('helaas u voldoet niet aan de eisen') 
+    else: print('helaas u voldoet niet aan de eisen')
+else: print('helaas u voldoet niet aan de eisen')  
+    
+    
